32d3e1ad5cee025f56f970b5cf59e0b4cde65893
caohuatao <351995664@qq.com>
2020-06-24 16:37:04 +0800

    更新：文档设置

--- docs/demo/index.vue
+++ docs/demo/index.vue
@@ -514,9 +514,10 @@
   body,
   html,
   #app {
-    width: 100%!important;
-    height: 100%!important;
+    width  : 100% !important;
+    height : 100% !important;
   }
+
   #super-flow-demo {
     width  : 100%;
     height : 100%;
